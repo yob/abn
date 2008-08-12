fe5ab71d325de7371824568d03bfbc72a1ca4407
Bodaniel Jeanes <me@bjeanes.com>
2008-08-12 17:27:08 +1000

    added some comments for rdoc rake task and changed Version from a class to a module

--- lib/abn.rb
+++ lib/abn.rb
@@ -1,6 +1,6 @@
 class ABN
 
-  class Version #:nodoc:
+  module Version #:nodoc:
     Major = 1
     Minor = 2
     Tiny  = 0
@@ -8,10 +8,15 @@ class ABN
     String = [Major, Minor, Tiny].join('.')
   end
 
+  # Creates an ABN object representing the ABN number passed
+  # as the only parameter.
   def initialize(num)
     @number = num.to_s.tr ' ',''
   end
 
+  # Returns whether the current ABN class represents a
+  # valid ABN number according to a weighting
+  # algorithm (not checked against a datbase)
   def valid?
     return false unless @number.length == 11
 
@@ -26,10 +31,14 @@ class ABN
     sum % 89 == 0 ? true : false
   end
 
+  # Correctly formats the represented ABN if valid, else returns
+  # an empty string
   def to_s
     valid? ? "%s%s %s%s%s %s%s%s %s%s%s" % @number.split('') : ""
   end
 
+  # Accepts an ABN number as a String or Bignum and returns
+  # whether or not it is valid (not checked against a database)
   def self.valid?(abn)
     new(abn).valid?
   end
